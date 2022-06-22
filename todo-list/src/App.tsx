fcb16c8a78f9f34946530f479875bba1e0de7691
gerardambe <gerardambe@yahoo.com>
2022-06-22 08:25:43 +0000

    initial commit

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return <div className="App"> 
+  <span className='heading'>My Tasks</span>
+  <InputField />
+  </div>
+};
+
+export default App;
